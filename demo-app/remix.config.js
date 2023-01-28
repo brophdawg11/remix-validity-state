2ef19eee665f3a8ca9e2f509265a59e2249fd5f7
Nick McCurdy <nick@nickmccurdy.com>
2023-01-28 13:09:34 -0500

    chore: run Prettier on project (#47)
    Co-authored-by: Nick McCurdy <nick@nickmccurdy.com>

--- demo-app/remix.config.js
+++ demo-app/remix.config.js
@@ -2,10 +2,5 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-  ignoredRouteFiles: [".*"]
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
-  // publicPath: "/build/",
-  // devServerPort: 8002
+  ignoredRouteFiles: [".*"],
 };
